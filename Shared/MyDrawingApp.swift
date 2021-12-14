3d509d4b65bfb85608963f29a570e31db4501822
Ashish Langhe <95478770+Ashish-Langhe@users.noreply.github.com>
2021-12-14 20:44:43 +0530

    Initial Commit

---
+++ Shared/MyDrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDrawingApp.swift
+//  Shared
+//
+//  Created by user208584 on 12/14/21.
+//
+
+import SwiftUI
+
+@main
+struct MyDrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
