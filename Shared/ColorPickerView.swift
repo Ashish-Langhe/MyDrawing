eab0abcd1af97483a8b63b16054bbe80bede3a5a
Ashish Langhe <95478770+Ashish-Langhe@users.noreply.github.com>
2021-12-15 17:59:03 +0530

    Updated ColorPicker

---
+++ Shared/ColorPickerView.swift
@@ -0,0 +1,36 @@
+//
+//  ColorPickerView.swift
+//  MyDrawing
+//
+//  Created by user208584 on 12/15/21.
+//
+
+import SwiftUI
+
+struct ColorPickerView: View {
+    
+    //MARK: - PROPERTIES
+    let colors = [Color.red, Color.orange, Color.green, Color.blue, Color.purple]
+    @Binding var selectedColor: Color
+    
+    
+    var body: some View {
+        HStack {
+            ForEach(colors, id: \.self) { color in
+                Image(systemName: selectedColor == color ? Constants.Icons.recordCircleFill : Constants.Icons.circleFill)
+                    .foregroundColor(color)
+                    .font(.system(size: 16))
+                    .clipShape(Circle())
+                    .onTapGesture {
+                        selectedColor = color
+                    }
+            }
+        }
+    }
+}
+
+struct ColorPickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorPickerView(selectedColor: .constant(.blue))
+    }
+}
