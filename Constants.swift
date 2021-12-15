230e4d66b6548346588e612c47bc83955f11c1e3
Ashish Langhe <95478770+Ashish-Langhe@users.noreply.github.com>
2021-12-15 17:44:01 +0530

    Added Icon Constants

---
+++ Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  MyDrawing
+//
+//  Created by Ashish Langhe on 12/15/21.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Icons {
+        static let plusCircle = "plus.circle"
+        static let line3HorizontalCircleFill = "line.3.horizontal.circle.fill"
+        static let circle = "circle"
+        static let circleInsertFilled = "circle.insert.filled"
+        static let exclamationMarkCircle = "exclamationmark.circle"
+        static let recordCircleFill = "record.circle.fill"
+        static let trayCircleFill = "trayCircleFill"
+        static let circleFill = "circle.fill"
+    }
+}
